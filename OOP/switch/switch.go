b46c144e1e04ede5d65448e0f5d3c555ed972b63
Narachii <naoya706oxford@gmail.com>
2021-09-17 19:36:43 +0900

    Add switch

--- OOP/switch/switch.go
+++ OOP/switch/switch.go
@@ -1,1 +1,49 @@
-package _switch
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	gender string
+	age int
+}
+
+func main() {
+	err := addPerson("Tina", "Female", 20)
+	if err != nil {
+		fmt.Println("PersonAdd Error:" + err.Error())
+	}
+
+}
+func addPerson(args ...interface{})  error {
+	if len(args) > 3 {
+		return fmt.Errorf("Wrong number of argumens passed")
+	}
+
+	p := &person{}
+
+	for i, arg := range args {
+		switch i {
+		case 0:
+			name, ok := arg.(string)
+			if !ok {
+				return fmt.Errorf("Name is not passed as string")
+			}
+			p.name = name
+		case 1:
+			gender, ok := arg.(string)
+			if !ok {
+				return fmt.Errorf("Gender is not passed as string")
+			}
+			p.gender = gender
+		case 2:
+			age, ok := arg.(int)
+			if !ok {
+				return fmt.Errorf("Age is not passed as string")
+			}
+			p.age = age
+		}
+	}
+	fmt.Printf("Person struct is %+v\n", p)
+	return nil
+}
